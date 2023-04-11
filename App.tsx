0621f15110c1cf544a5195207c6f028d45ccd6a6
Ivan Santander <isantander@be4tech.co>
2023-04-11 08:02:28 -0500

    avance conexion bluetooth > android 12 OK

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   NativeEventEmitter,
   NativeModules,
@@ -16,6 +16,7 @@ import {
   Text,
   useColorScheme,
   View,
+  PermissionsAndroid
 } from 'react-native';
 
 import {
@@ -25,9 +26,10 @@ import {
 import { VitalCore } from "@tryvital/vital-core-react-native";
 import { HealthConfig, VitalHealth, VitalHealthEvents, VitalHealthReactNativeModule, VitalResource } from '@tryvital/vital-health-react-native';
 import { VitalClient } from '@tryvital/vital-node';
-import {VITAL_API_KEY, VITAL_ENVIRONMENT, VITAL_REGION, VITAL_USER_ID} from './Environment';
+import { VITAL_API_KEY, VITAL_ENVIRONMENT, VITAL_REGION, VITAL_USER_ID } from './Environment';
 import { Brand, Cancellable, DeviceKind, DeviceModel, VitalDevicesManager } from '@tryvital/vital-devices-react-native';
-import {PERMISSIONS, requestMultiple} from 'react-native-permissions';
+import { check, request,PERMISSIONS, requestMultiple, RESULTS } from 'react-native-permissions';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const { VitalDevicesReactNative } = NativeModules;
 
@@ -43,29 +45,25 @@ VitalHealth.configureClient(
   VITAL_REGION,
   true,
 ).then(() => {
-  console.log('VitalHealth configured client');
   VitalHealth.configure(new HealthConfig()).then(() => {
-    console.log('VitalHealth configured');
     VitalHealth.setUserId(VITAL_USER_ID)
       .then(() => {
-        console.log('VitalHealth setUserId');
         VitalHealth.askForResources([VitalResource.Steps])
           .then(() => {
-            console.log('VitalHealth asked for resources');
             VitalHealth.syncData([VitalResource.Steps])
               .then(() => {
                 console.log('VitalHealth synced data');
               })
               .catch((error: any) => {
-                console.log(error);
+                console.error(error);
               });
           })
           .catch((error: any) => {
-            console.log(error);
+            console.error(error);
           });
       })
       .catch((error: any) => {
-        console.log(error);
+        console.error(error);
       });
   });
 });
@@ -73,7 +71,10 @@ VitalHealth.configureClient(
 const healthEventEmitter = new NativeEventEmitter(VitalHealthReactNativeModule);
 
 healthEventEmitter.addListener(VitalHealthEvents.statusEvent, (event: any) => {
-  console.log(VitalHealthEvents.statusEvent, event);
+  console.log("addListener",VitalHealthEvents);
+  // console.log("VitalHealthEvents.statusEvent",VitalHealthEvents.statusEvent);
+  // console.log("event", event);
+  // console.log("addListener_VitalHealthEvents.statusEvent, event",VitalHealthEvents.statusEvent, event);
 });
 
 const vitalDevicesManager = new VitalDevicesManager((module) => new NativeEventEmitter(module));
@@ -85,6 +86,136 @@ let bleSimulator: DeviceModel = {
   kind: DeviceKind.GlucoseMeter,
 };
 
+// async function requestBluetoothScanPermission() {
+//   try {
+//     if (Platform.OS == 'android') {
+//       const granted = await PermissionsAndroid.request(
+//         PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
+//       );
+//       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+//         console.log('ACCESS_COARSE_LOCATION',PermissionsAndroid.RESULTS.GRANTED);
+//       } else {
+//         console.log('ACCESS_COARSE_LOCATION false');
+//       }
+//     } else {
+//       console.log('Dispositivo != android');
+//     }
+//   } catch (error) {
+//     console.error('Error al solicitar permiso ACCESS_COARSE_LOCATION', error);
+//   }
+// }
+
+
+
+// async function requestBluetoothScanPermission() {
+//   try {
+//     const result = await check(
+//       Platform.OS === 'android'
+//         ? PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION
+//         : PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL,
+//     );
+//     if (result === RESULTS.GRANTED) {
+//       console.log('Permiso ACCESS_COARSE_LOCATION otorgado');
+//     }else if(result == 'denied'){
+//       const result = await request(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION);
+//       if (result === RESULTS.GRANTED) {
+//         console.log('Permiso ACCESS_COARSE_LOCATION otorgado');
+//       }else{
+//         console.log('Permiso ACCESS_COARSE_LOCATION NOO otorgado result->', result);
+//       }
+//     }
+//   } catch (error) {
+//     console.error('Error al solicitar permiso ACCESS_COARSE_LOCATION', error);
+//   }
+// }
+
+
+const bluetoothPermission = Platform.OS === 'android' ? PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION : PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL;
+
+check(bluetoothPermission)
+  .then((result) => {
+    switch (result) {
+      case RESULTS.UNAVAILABLE:
+        console.log('Permiso no disponible en este dispositivo');
+        break;
+      case RESULTS.DENIED:
+        console.log('Permiso denegado, solicitando permiso...');
+        requestPermission();
+        break;
+      case RESULTS.GRANTED:
+        console.log('Permiso concedido 11');
+        break;
+      case RESULTS.BLOCKED:
+        console.log('El permiso ha sido bloqueado, deberías habilitarlo manualmente');
+        break;
+    }
+  })
+  .catch((error) => console.log(error));
+
+
+  const requestPermission = () => {
+    request(bluetoothPermission)
+      .then((result) => {
+        switch (result) {
+          case RESULTS.UNAVAILABLE:
+            console.log('Permiso no disponible en este dispositivo');
+            break;
+          case RESULTS.DENIED:
+            console.log('Permiso denegado');
+            break;
+          case RESULTS.GRANTED:
+            console.log('Permiso concedido 22');
+            break;
+          case RESULTS.BLOCKED:
+            console.log('El permiso ha sido bloqueado, deberías habilitarlo manualmente');
+            break;
+          default:
+            console.log('Permiso default', result);
+
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+  
+
+requestMultiple([
+  PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+]).then(statuses => {
+  console.log("statuses",statuses);
+  if (
+      statuses[PERMISSIONS.ANDROID.BLUETOOTH_SCAN ] === 'granted'
+  ) {
+    console.log("@@@ Start1 scanning for device type: " + bleSimulator.name)
+
+    var scanner: Cancellable | null
+    scanner = vitalDevicesManager.scanForDevice(
+      bleSimulator,
+      {
+        onDiscovered: (device) => {
+          console.log("@@@2 Discovered device: " + device.name + " (id = " + device.id + ")")
+          scanner?.cancel()
+
+          console.log("@@@3 Start pairing device: " + device.name + " (id = " + device.id + ")")
+          vitalDevicesManager.pairDevice(device.id)
+            .then(() => {
+              console.log("@@@4 Successfully paired device: " + device.name + " (id = " + device.id + ")")
+              console.log("@@@5 Start reading from device: " + device.name + " (id = " + device.id + ")")
+
+              return vitalDevicesManager.readGlucoseMeter(device.id)
+            })
+            .then((samples) => {
+              console.log("@@@6 Read " + samples.length + " samples from device: " + device.name + " (id = " + device.id + ")")
+              console.log(samples)
+            })
+            .catch((error) => console.log('1',error))
+        },
+        onError: (error) => console.log('2',error)
+      }
+    )
+  }
+});
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -95,9 +226,10 @@ function App(): JSX.Element {
 
   const [isVitalReady, setIsVitalReady] = useState(false);
 
-  useEffect(() => {
-    console.log('useEffect')
-  }, []);
+  // useEffect(() => {
+  //   requestBluetoothPermission();
+  //   console.log('useEffect')
+  // }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
--- Environment.tsx
+++ Environment.tsx
@@ -1,5 +1,10 @@
 // Vital API Key
-export const VITAL_API_KEY = 'sk_us_r47FqsX49GXMwBVM4Og4yeUPoX8q3xaZx6d3nlrTSJg';
+
+// IVAN
+// export const VITAL_API_KEY = 'sk_us_r47FqsX49GXMwBVM4Og4yeUPoX8q3xaZx6d3nlrTSJg';
+
+// BE4CARE
+export const VITAL_API_KEY = 'sk_us_CBAcPPiwDnporPRIFCZRqKwrHYKU4Rw3Yc4ez6_zDuI';
 
 // Environment: sandbox, production
 export const VITAL_ENVIRONMENT = 'sandbox';
@@ -8,6 +13,6 @@ export const VITAL_ENVIRONMENT = 'sandbox';
 export const VITAL_REGION = 'us';
 
 // Vital User ID (UUID)
-export const VITAL_USER_ID = '4fde1154-fd8a-4571-80c5-e27858a7c279';
+export const VITAL_USER_ID = '4f60d2b4-e279-46bc-8ed0-81b55255d73c';
 
 export default function() {}
