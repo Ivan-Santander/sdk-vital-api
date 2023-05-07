d46cb9e534c3eb08abf948e0a0db17eb79a8f96c
Ivan Santander <isantander@be4tech.co>
2023-05-07 09:39:39 -0500

    avance 07-05-23

--- App.tsx
+++ App.tsx
@@ -1,76 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { useEffect, useState } from 'react';
 import {
   Button,
-  FlatList,
   Linking,
   Modal,
   SafeAreaView,
-  ScrollView,
+  SectionList,
   StyleSheet,
   Text,
   TextInput,
-  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import { useCallback } from 'react';
 
-import { useVitalLink } from '@tryvital/vital-link';
 
 const API_URL = "https://be4care-vital.herokuapp.com"
-
-// const userKey = '4f60d2b4-e279-46bc-8ed0-81b55255d73c';
-const userKey = '8181c51a-529d-4d18-856e-60777264005a';
-
-// export const VITAL_API_KEY = 'sk_us_CBAcPPiwDnporPRIFCZRqKwrHYKU4Rw3Yc4ez6_zDuI';
-
-const getTokenFromBackend = async (userKey: string, env: string) => {
-  const resp = await fetch(`${API_URL}/token/${userKey}`);
-  const data = await resp.json();
-  return data;
-  // const resp = await fetch(API_URL)
-  // return await resp.json()
-}
 
 
 function App(): JSX.Element {
   const [showModal, setShowModal] = useState(false);
   const isDarkMode = useColorScheme() === 'dark';
   const [isLoading, setLoading] = useState(false);
-  const [newData, setNewData] = useState([]);
-  const [newData2, setNewData2] = useState([]);
+  //Data actividad
+  const [dataActivity, setDataActivity] = useState([]);
+  //Data completa
+  const [dataUsers, setDataUser] = useState([]);
+  //Data all users
+  const [allUsers, setAllUsers] = useState([]);
   const [nombre, setNombre] = useState("");
+  const [userKey, setUserKey]:any = useState('8181c51a-529d-4d18-856e-60777264005a');
 
-  const onSuccess = useCallback(metadata => {
-    // Device is now connected.
-  }, []);
 
-  const onExit = useCallback(metadata => {
-    // User has quit the link flow.
-  }, []);
-
-  const onError = useCallback(metadata => {
-    // Error encountered in connecting device.
-  }, []);
-
-  const config = {
-    onSuccess,
-    onExit,
-    onError,
-    env: "sandbox"
-  };
-
-  const { open, ready, error } = useVitalLink(config);
-
+  const getTokenFromBackend = async (userKey: string, env: string) => {
+    const resp = await fetch(`${API_URL}/token/${userKey}`);
+    const data = await resp.json();
+    return data;
+  }
 
   const handleVitalOpen = async () => {
     setLoading(true);
@@ -80,14 +46,23 @@ function App(): JSX.Element {
     setLoading(false);
   };
 
+  const viewAllUsers = async () => {
+    setLoading(true);
+    const resp = await fetch(`${API_URL}/users/`);
+    const data = await resp.json();
+    console.log("data_viewUsers",data)
+    setAllUsers(data)
+    console.log('allUsers',allUsers)
+    setLoading(false);
+  }
 
-  const VitalAnalize = async () => {
+  const VitalAnalizeActivity = async () => {
     setLoading(true);
     const resp = await fetch(`${API_URL}/summary/activity/${userKey}?start_date=2023-04-01T19:48:37.078Z&end_date=2023-04-11T19:48:37.078Z`);
     const data = await resp.json();
       // Organizar datos
     const summary = data.activity;
-    const newData = [];
+    const newData:any = [];
     summary.forEach(item => {
       const newItem = {
         calendar_date: item.calendar_date,
@@ -103,8 +78,8 @@ function App(): JSX.Element {
 
     // Usar los datos organizados
     console.log("newData", newData);
-    setNewData(newData);
-    setLoading(false);
+    setDataActivity(newData);
+    console.log('dataActivity',dataActivity)
     setLoading(false);
   };
 
@@ -131,13 +106,12 @@ function App(): JSX.Element {
 
     // // Usar los datos organizados
     // console.log("newData2", newData2);
-    setNewData(newData2);
-    setLoading(false);
+    setDataUser(data);
     setLoading(false);
   };
   // 
 
-  const CreateUserVital = async (nombre) => {
+  const CreateUserVital = async (nombre:any) => {
     const resp = await fetch(`${API_URL}/user/`, {
       method: "POST",
       headers: {
@@ -162,48 +136,66 @@ function App(): JSX.Element {
   }, []);
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-           <Text numberOfLines={1}>{"\n"}</Text>
-           <Text style={styles.texto}>Hi Humans</Text>
-           <Button title="Crear nuevo usuario" onPress={() => setShowModal(true)} />
-           <Text numberOfLines={1}>{"\n"}</Text>
-           <Button title="Conectar dispositivo" onPress={handleVitalOpen} />
-           <Text numberOfLines={1}>{"\n"}</Text>
-           <Button title="Ver datos completos" onPress={VitalAnalizeData} />
-           <Text numberOfLines={1}>{"\n"}</Text>
-           <Button  title="Ver actividad" onPress={VitalAnalize} />
-           <Text numberOfLines={1}>{"\n"}</Text>
-           <FlatList
-              data={newData}
-              renderItem={({ item }) => (
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                  <Text>{item.calendar_date}</Text>
-                  <Text>{item.calories_active}</Text>
-                  <Text>{item.steps}</Text>
-                  <Text>{item.platform}</Text>
-                </View>
-              )}
-              keyExtractor={(item, index) => index.toString()}
-            />
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text numberOfLines={1}>{"\n"}</Text>
+      <Text style={styles.texto}>B4collect v0.0.1</Text>
+      <Text numberOfLines={1}>{"\n"}</Text>
 
-            <Modal visible={showModal}>
-              <View>
-                <Text>Ingrese los datos:</Text>
-                <TextInput placeholder="Nombre" onChangeText={text => setNombre(text)} />
-                <Button title="Enviar" onPress={() => CreateUserVital(nombre)} />
-                <Button title="Cancelar" onPress={() => setShowModal(false)} />
+      {!userKey ? (
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          flex: 1,
+        }}>
+         <Button title="Ver usuarios" onPress={() => viewAllUsers()} />
+         <Text numberOfLines={1}>{"\n"}</Text>
+         <Button title="Crear nuevo usuario" onPress={() => setShowModal(true)} />
+         <Text numberOfLines={1}>{"\n"}</Text>
+         <Modal visible={showModal}>
+            <View>
+              <Text>Ingrese los datos:</Text>
+              <TextInput placeholder="Nombre" onChangeText={text => setNombre(text)} />
+              <Button title="Enviar" onPress={() => CreateUserVital(nombre)} />
+              <Button title="Cancelar" onPress={() => setShowModal(false)} />
+            </View>
+          </Modal>
+      </View>
+      ) : (
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          flex: 1,
+        }}>
+         <Button title="Cambiar usuario" onPress={() => {
+          console.log("userKey",userKey)
+          setUserKey(undefined);
+          console.log("userKey",userKey)
+         }} />
+         <Text numberOfLines={1}>{"\n"}</Text>
+         <Button title="Conectar dispositivo" onPress={handleVitalOpen} />
+         <Text numberOfLines={1}>{"\n"}</Text>
+         <Button title="Ver datos completos" onPress={VitalAnalizeData} />
+         <Text numberOfLines={1}>{"\n"}</Text>
+         <Button title="Ver actividad" onPress={VitalAnalizeActivity} />
+         <Text numberOfLines={1}>{"\n"}</Text>
+         <SectionList
+            sections={[{ data: dataActivity }]}
+            renderItem={({ item }) => (
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text>{item.calendar_date}</Text>
+                <Text>{item.calories_active}</Text>
+                <Text>{item.steps}</Text>
+                <Text>{item.platform}</Text>
               </View>
-            </Modal>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+      </View>
+      )}
 
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -217,6 +209,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  visible: {
+    display: 'flex'
+  },
+  hidden: {
+    display: 'none'
+  }
   });
 
 export default App;
